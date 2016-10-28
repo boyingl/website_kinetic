7cfb7ec1f79c08b37c80ff8b369d0058a0387180
Ashley Liu <liuboying@hotmail.com>
2016-10-27 20:00:30 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+<body ng-app="">
+
+<div ng-include="'footer.html'"></div>
+    
+    <?php echo "hello world" ?>
+
+</body>
+</html>
